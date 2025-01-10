443ae26ff83949a048c910e415a8b7647e18456f
Enndfp <3193947435@qq.com>
2025-01-10 21:38:17 +0800

    feat: ✨ 实现用户自动登录、布局切换、请求优化及权限控制

---
+++ generated/services/FileControllerService.ts
@@ -0,0 +1,86 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { BaseResponse_string_ } from "../models/BaseResponse_string_";
+import type { CancelablePromise } from "../core/CancelablePromise";
+import { OpenAPI } from "../core/OpenAPI";
+import { request as __request } from "../core/request";
+
+export class FileControllerService {
+  /**
+   * testDownloadFile
+   * @param filepath filepath
+   * @returns any OK
+   * @throws ApiError
+   */
+  public static testDownloadFileUsingGet(
+    filepath?: string
+  ): CancelablePromise<any> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/file/test/download",
+      query: {
+        filepath: filepath,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
+   * testUploadFile
+   * @param file file
+   * @returns BaseResponse_string_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static testUploadFileUsingPost(
+    file: Blob
+  ): CancelablePromise<BaseResponse_string_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/file/test/upload",
+      formData: {
+        file: file,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
+   * uploadFile
+   * @param file file
+   * @param biz
+   * @returns BaseResponse_string_ OK
+   * @returns any Created
+   * @throws ApiError
+   */
+  public static uploadFileUsingPost(
+    file: Blob,
+    biz?: string
+  ): CancelablePromise<BaseResponse_string_ | any> {
+    return __request(OpenAPI, {
+      method: "POST",
+      url: "/api/file/upload",
+      query: {
+        biz: biz,
+      },
+      formData: {
+        file: file,
+      },
+      errors: {
+        401: `Unauthorized`,
+        403: `Forbidden`,
+        404: `Not Found`,
+      },
+    });
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <template>
   <div id="app">
-    <BasicLayOut />
+    <template v-if="route.path.startsWith('/user')">
+      <router-view />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
   </div>
 </template>
 
 <style></style>
 <script setup lang="ts">
-import BasicLayOut from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+import BasicLayout from "@/layouts/BasicLayout.vue";
+
+const route = useRoute();
 
 /**
  * 全局初始化函数，有全局单次调用的代码
  */
 const doInit = () => {
-  console.log("hello 欢迎来到我的项目");
+  console.log("Hello 欢迎来到我的项目!");
 };
 
 onMounted(() => {
   doInit();
-});
-
-const router = useRouter();
-const store = useStore();
-router.beforeEach((to, from, next) => {
-  // 仅管理员可见，判断当前用户是否有权限
-  if (to.meta.access === "canAdmin") {
-    if (store.state.loginUser?.role !== "admin") {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
