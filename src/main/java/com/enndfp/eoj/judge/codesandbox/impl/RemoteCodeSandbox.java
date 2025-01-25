6f23472a8e4d477cc0c5c2bc4ca0326cd47966af
Enndfp <3193947435@qq.com>
2025-01-25 20:06:40 +0800

    feat: ✨ 实现调用远程沙箱

--- src/main/java/com/enndfp/eoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/enndfp/eoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,8 +1,13 @@
 package com.enndfp.eoj.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.enndfp.eoj.common.ErrorCode;
+import com.enndfp.eoj.exception.BusinessException;
 import com.enndfp.eoj.judge.codesandbox.CodeSandbox;
 import com.enndfp.eoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.enndfp.eoj.judge.codesandbox.model.ExecuteCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远程代码沙箱
@@ -11,10 +16,30 @@ import com.enndfp.eoj.judge.codesandbox.model.ExecuteCodeResponse;
  */
 public class RemoteCodeSandbox implements CodeSandbox {
 
+    /**
+     * 请求头
+     */
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    /**
+     * 请求密钥
+     */
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
+                .body(json)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 
 }
