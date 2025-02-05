82dab689ce7c08ca644191908afb5cc2c39587f5
Enndfp <3193947435@qq.com>
2025-02-05 13:47:24 +0800

    feat: ✨ 初始化用户服务模块

--- eoj-backend-user-service/src/main/java/com/enndfp/eojbackenduserservice/EojBackendUserServiceApplication.java
+++ eoj-backend-user-service/src/main/java/com/enndfp/eojbackenduserservice/EojBackendUserServiceApplication.java
@@ -1,9 +1,13 @@
 package com.enndfp.eojbackenduserservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.enndfp")
+@MapperScan("com.enndfp.eojbackenduserservice.mapper")
 public class EojBackendUserServiceApplication {
 
     public static void main(String[] args) {
